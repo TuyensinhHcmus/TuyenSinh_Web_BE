0b7468825a1dc143e30748b9e60b5c946f2211f0
fannatic2000 <55821590+fannatic2000@users.noreply.github.com>
2022-03-08 13:34:55 +0700

    Duy-commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://tuyensinhhcmus:Y64oNI6ptREGRyN6@tuyensinhhcmus.2t0eq.mongodb.net/TuyenSinhHCMUS'
+    )
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
