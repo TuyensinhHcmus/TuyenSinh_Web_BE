178cd5e9fdbd407e8aa9b6e0f28a1639d610f2cf
162808-PQLUONG\HP <quocluong2503@gmail.com>
2022-06-09 23:52:14 +0700

    api type program

--- src/typePrograms/typePrograms.controller.ts
+++ src/typePrograms/typePrograms.controller.ts
@@ -7,10 +7,12 @@ import {
   Patch,
   Delete,
   Query,
+  Request,
 } from '@nestjs/common';
 
 import { TypeProgramsService } from './typePrograms.service';
 import { typeProgram } from './typeProgram.entity';
+import { TypeProgramDto } from './dto/add-typeprogram-dto';
 
 @Controller('type-programs')
 export class TypeProgramsController {
@@ -38,6 +40,29 @@ export class TypeProgramsController {
   @Get()
   async getAllTypePrograms(): Promise<typeProgram[]> {
     const typePrograms = await this.typeProgramsService.getTypePrograms();
+    return typePrograms;
+  }
+
+  @Get("/:id")
+  async getTypeProgramById(@Param("id") id: string): Promise<typeProgram> {
+    const typePrograms = await this.typeProgramsService.getTypeProgramById(id);
+    return typePrograms;
+  }
+
+  @Post("/add")
+  async addTypeProgram(@Body() infor: TypeProgramDto): Promise<typeProgram> {
+    const typePrograms = await this.typeProgramsService.addTypeProgram(infor);
+    return typePrograms;
+  }
+
+  @Post("/delete")
+  async delteTypeProgram(@Body("typeProgramId") id: string): Promise<any> {
+    const res = await this.typeProgramsService.deleteTypeProgram(id);
+    return res;
+  }
+  @Post("/update")
+  async updateTypeProgram(@Body() infor: TypeProgramDto): Promise<any> {
+    const typePrograms = await this.typeProgramsService.updateTypeProgram(infor);
     return typePrograms;
   }
 }
