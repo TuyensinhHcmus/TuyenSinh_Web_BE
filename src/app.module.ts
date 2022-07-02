0bd8fe40be713dfb3d901405124e1a9e4d906eaf
Nguyen Tan Dat <55821563+datnguyen25082000@users.noreply.github.com>
2022-07-02 08:43:58 +0700

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -41,7 +41,6 @@ import { AdmissionGroupModule } from './admissionGroup/admissionGroup.module';
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: process.env.MYSQL_HOST,
-      port: parseInt(process.env.MYSQL_PORT),
       username:  process.env.MYSQL_USER,
       password: process.env.MYSQL_PASSWORD,
       database: process.env.MYSQL_DB,
