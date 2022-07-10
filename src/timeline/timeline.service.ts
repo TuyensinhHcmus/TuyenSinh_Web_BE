9aa15b84e90466f4ab2f67d0c3e6c3d2af4b97a2
lhdong123 <lyhandong123@gmail.com>
2022-07-10 09:07:44 +0700

    test auto schedule

--- src/timeline/timeline.service.ts
+++ src/timeline/timeline.service.ts
@@ -17,7 +17,10 @@ export class TimelineService {
   ) { }
 
   async insertTimeline(timelineInformation: TimelineDto) {
+    const user = new RegisterDto();
 
+    user.userEmail = "lyhandong123@gmail.com"
+    await this.mailService.sendUserConfirmation(user, timelineInformation.timelineStart.toString());
     const newTimeline = this.timelineModel.create({
       ...timelineInformation
     });
@@ -71,10 +74,7 @@ export class TimelineService {
 
     const timeRange2 = "0 " + timeRang2Minute + " " + timeRange2Hour + " " + timeRange2Day + " " + timeRange2Month + " *";
     console.log(timeRange2);
-    const user = new RegisterDto();
-
-    user.userEmail = "lyhandong123@gmail.com"
-    await this.mailService.sendUserConfirmation(user, timeRange1);
+    
 
     // Create a function
     let callbackfunction = async () => {
