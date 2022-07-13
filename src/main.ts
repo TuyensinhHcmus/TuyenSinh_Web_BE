9a14cebc98b8c2b2a243bd7f75ab3a8f5ab860b6
Nguyen Tan Dat <datnguyen25082000@gmail.com>
2022-07-13 09:01:05 +0700

    datnt fix limit

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,21 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as cors from 'cors';
 //import { AtGuard } from './common/guards';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.use(bodyParser.json({
+    limit: '50mb'
+  }));
+  
+  app.use(bodyParser.urlencoded({
+    limit: '50mb',
+    parameterLimit: 100000,
+    extended: true 
+  }));
+  
   // to validation data when register
   app.useGlobalPipes(new ValidationPipe());
   // app.useGlobalGuards(new AtGuard())
